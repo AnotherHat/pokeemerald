78d267dc0ebc1fbf6930e90d2135b70112affc6a
Ryuhouji <bcummans@hotmail.com>
2022-07-03 02:44:30 -0400

    Added ability to automatically append variable values to a printed string

--- src/start_menu.c
+++ src/start_menu.c
@@ -565,7 +565,7 @@ void ShowStartMenu(void)
     ScriptContext2_Enable();
 }
 
-extern void DebugPrint(const u8 *buffer);
+extern void DebugPrint(const u8 *buffer, int count, ...);
 static bool8 HandleStartMenuInput(void)
 {
     if (JOY_NEW(DPAD_UP))
@@ -606,7 +606,7 @@ static bool8 HandleStartMenuInput(void)
     {
         RemoveExtraStartMenuWindows();
         HideStartMenu();
-        DebugPrint((const u8[]) _("Closed the start menu."));
+        DebugPrint((const u8[]) _("Play Time:"), 2, gSaveBlock2Ptr->playTimeHours, gSaveBlock2Ptr->playTimeMinutes);
         return TRUE;
     }
 
